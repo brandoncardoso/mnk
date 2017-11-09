a1398c0569fd41f4543cd35657ea9d726930dc4b
Brandon Cardoso <puzzlepazuru@gmail.com>
2017-11-08 22:47:53 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mnk_game',
+      version='0.1',
+      description='A connect k-in-a-row game on a m*n grid.',
+      url='http://github.com/brandoncardoso/mnk_game',
+      author='Brandon Cardoso',
+      license='MIT',
+      packages=['mnk_game'],
+      zip_safe=False)
